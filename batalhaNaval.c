b74c3a1e13cbbd2d0a29ce17279013214faf133f
Felippe Bueno <felippebueno25@gmail.com>
2025-09-17 14:25:02 +0000

    Nivel novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void imprimeMatriz(int matriz[10][10]){
+    int i,j;
+    printf("-----------------------\n");
+    printf("     Batalha Naval\n");
+    printf("-----------------------\n");
+    printf("    A B C D E F G H I J\n");
+    for (i = 0; i < 10; i++){
+        printf("%2d  ",i+1);
+        for (j = 0; j < 10; j++){
+            printf("%d ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("-----------------------\n");
+}
+
+void navioVertical(int matriz[10][10]){
+    int i;
+    for (i = 0; i < 3; i++){
+        matriz[i+5][7] = 3;
+    }
+}
+
+void navioHorizontal(int matriz[10][10]){
+    int i;
+    for (i = 0; i < 3; i++){
+        matriz[2][i+3] = 3;
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int matriz[10][10]={0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // Imprime matriz zerada
+    imprimeMatriz(matriz);
+    printf("\n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Posiciona Navio 1 - horizontal
+    navioHorizontal(matriz);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Posiciona Navio 2  - vertical
+    navioVertical(matriz);
+       
+    // Imprime matriz com navios posicionados
+    imprimeMatriz(matriz);
+
 
     return 0;
 }
