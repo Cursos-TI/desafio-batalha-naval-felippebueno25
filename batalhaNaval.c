a179e1cc130c78d7741f732c40291e7596ec60e9
Felippe Bueno <felippebueno25@gmail.com>
2025-09-17 15:50:18 +0000

    Adiciona verificações de limites e sobreposição ao posicionar navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -19,34 +19,49 @@ void imprimeMatriz(char matriz[LINHA][COLUNA]){
 
 void navioVertical(int linha, int coluna, char matriz[LINHA][COLUNA]){
     int i;
-    if (matriz[linha + i][coluna] != '0') {
-        printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha + i, coluna);
+    if (linha > 9 - 2 && coluna <= 9) { // Verifica se o navio cabe na vertical
+        printf("Erro: Navio nao cabe na posicao (%d, %d)!\n", linha, coluna);
         return;
-    }
-    for (i = 0; i < 3; i++){
-        matriz[linha + i][coluna] = 'V';
-    }
+        for (i = 0; i < 3; i++){
+            if (matriz[linha + i][coluna] != '0') {
+                printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha + i, coluna);
+                return;
+            }
+            matriz[linha + i][coluna] = 'V';
+        }
+    } else {
+        printf("Erro: Navio nao cabe na posicao (%d, %d)!\n", linha, coluna);
+}
 }
 
 void navioHorizontal(int linha, int coluna, char matriz[LINHA][COLUNA]){
     int i;
-    if (matriz[linha][coluna + i] != '0') {
-        printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha, coluna + i);
-        return;
-    }
-    for (i = 0; i < 3; i++){
+    if (linha <= 9 && coluna <= 9 - 2) // Verifica se o navio cabe na horizontal
+    {
+        for (i = 0; i < 3; i++){
+        if (matriz[linha][coluna + i] != '0') {
+            printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha, coluna + i);
+            return;
+        }
         matriz[linha][coluna + i] = 'H';
     }
+    } else {
+        printf("Erro: Navio nao cabe na posicao (%d, %d)!\n", linha, coluna);
+    }   
 }
 
 void navioDiagonal(int linha, int coluna, char matriz[LINHA][COLUNA]){
     int i;
-    for (i = 0; i < 3; i++){
-        if (matriz[linha + i][coluna + i] != '0') {
-            printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha + i, coluna + i);
-            return;
-        }
-        matriz[linha + i][coluna + i] = 'D';
+    if (linha <= 9 - 2 && coluna <= 9 - 2) {  // Verifica se o navio cabe na diagonal
+        for (i = 0; i < 3; i++){
+            if (matriz[linha + i][coluna + i] != '0') {
+                printf("Erro: Posicao (%d, %d) ja ocupada!\n", linha + i, coluna + i);
+                return;
+            }
+            matriz[linha + i][coluna + i] = 'D';
+        } 
+    } else {
+        printf("Erro: Navio nao cabe na posicao (%d, %d)!\n", linha, coluna);
     }
 }
 
@@ -68,12 +83,15 @@ int main() {
     navioHorizontal(2, 3, matriz);
 
     // Posiciona Navio 2  - vertical
+    navioVertical(2, 3, matriz);
     navioVertical(4, 7, matriz);
 
     // Posiciona Navio 3 - diagonal
     // Teste sobreposição
-    // navioDiagonal(4, 7, matriz);
-    navioDiagonal(4, 2, matriz);
+    navioDiagonal(4, 7, matriz);
+    // Teste limite
+    navioDiagonal(8, 2, matriz);
+    navioDiagonal(7, 2, matriz);
        
     // Imprime matriz com navios posicionados
     imprimeMatriz(matriz);
